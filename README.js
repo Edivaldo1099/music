9c91d77f7b3d4c8c6a854f0851a6535a2fc2a824
Teste <102331900+Edivaldo1099@users.noreply.github.com>
2022-03-29 01:25:18 -0300

    musicstylesy
    #EXTM3U
    
    #### SÃO PAULO #######
    ######## RADIOS ######
    
    
    #EXTINF:-1 tvg-logo="https://img.radios.com.br/radio/lg/radio8829_1542619624.jpg" group-title="SP", JOVEM PAN FM
    https://24963.live.streamtheworld.com:443/JP_SP_FMAAC.aac?type=.flv
    
    #EXTINF:-1 tvg-logo="https://img.radios.com.br/radio/lg/radio8760_1565178044.png" group-title="SP",Rádio Hot 107 FM
    http://shout35.crossradio.com.br:8116/stream?type=.flv
    
    #EXTINF:-1 tvg-logo="https://img.radios.com.br/radio/lg/radio13955_1532353280.png" group-title="SP",Rádio Mix 106.3 FM
    https://24063.live.streamtheworld.com:443/MIXFM_SAOPAULOAAC.aac?type=.flv
    
    #EXTINF:-1 tvg-logo="https://img.radios.com.br/radio/lg/radio10358_1526480428.jpg" group-title="SP",Rádio Band FM 96.1 FM
    https://18693.live.streamtheworld.com/BANDFM_SPAAC/HLS/72dd4002-61a2-4a69-9372-fc23aa15f860/0/playlist.m3u8
    
    #EXTINF:-1 tvg-logo="https://img.radios.com.br/radio/lg/radio9505_1574106966.jpg" group-title="SP",Rádio Antena 1 94.7 FM
    https://antenaone.crossradio.com.br/stream/1;?type=.flv
    
    #EXTINF:-1 tvg-logo="https://img.radios.com.br/radio/lg/radio8803_1578516680.png"group-title="SP",Rádio Transamérica 100.1 FM
    https://25003.live.streamtheworld.com:443/RT_SPAAC.aac?type=.flv
    
    #EXTINF:-1 tvg-logo="https://img.radios.com.br/radio/lg/radio31289_1439401796.jpg" group-title="SP",89 FM Rádio Rock
    https://14013.live.streamtheworld.com:443/RADIO_89FM_ADP.aac
    
    #EXTINF:-1 tvg-logo="https://img.radios.com.br/radio/lg/radio24477_1579261235.jpg" group-title="SP",Rádio BandNews SP 96.9 FM
    https://26503.live.streamtheworld.com/BANDNEWSFM_SPAAC/HLS/05b3785a-6e2f-4919-a995-7e582a1a0bdc/0/playlist.m3u8
    
    #EXTINF:-1 tvg-logo="" group-title="RADIOS",
    
    #EXTINF:-1 tvg-logo="" group-title="RADIOS",
    
    #EXTINF:-1 tvg-logo="" group-title="RADIOS",
    
    #EXTINF:-1 tvg-logo="" group-title="RADIOS",
    
    
    
    ### RIO DE JANEIRO #####
    ######## RADIOS ######
    
    
    #EXTINF:-1 tvg-logo="https://img.radios.com.br/radio/lg/radio13213_1559217338.png" group-title="RJ",Rádio FM O Dia 100.5
    http://streaming.livespanel.com:20000/live?type=.flv
    
    
    #EXTINF:-1 tvg-logo="" group-title="SP",
    
    #EXTINF:-1 tvg-logo="" group-title="RADIOS",
    
    #EXTINF:-1 tvg-logo="" group-title="RADIOS",
    
    #EXTINF:-1 tvg-logo="" group-title="RADIOS",
    
    #EXTINF:-1 tvg-logo="" group-title="RADIOS",

---
+++ README.js
@@ -0,0 +1,57 @@
+#EXTM3U
+
+#### SÃO PAULO #######
+######## RADIOS ######
+
+
+#EXTINF:-1 tvg-logo="https://img.radios.com.br/radio/lg/radio8829_1542619624.jpg" group-title="SP", JOVEM PAN FM
+https://24963.live.streamtheworld.com:443/JP_SP_FMAAC.aac?type=.flv
+
+#EXTINF:-1 tvg-logo="https://img.radios.com.br/radio/lg/radio8760_1565178044.png" group-title="SP",Rádio Hot 107 FM
+http://shout35.crossradio.com.br:8116/stream?type=.flv
+
+#EXTINF:-1 tvg-logo="https://img.radios.com.br/radio/lg/radio13955_1532353280.png" group-title="SP",Rádio Mix 106.3 FM
+https://24063.live.streamtheworld.com:443/MIXFM_SAOPAULOAAC.aac?type=.flv
+
+#EXTINF:-1 tvg-logo="https://img.radios.com.br/radio/lg/radio10358_1526480428.jpg" group-title="SP",Rádio Band FM 96.1 FM
+https://18693.live.streamtheworld.com/BANDFM_SPAAC/HLS/72dd4002-61a2-4a69-9372-fc23aa15f860/0/playlist.m3u8
+
+#EXTINF:-1 tvg-logo="https://img.radios.com.br/radio/lg/radio9505_1574106966.jpg" group-title="SP",Rádio Antena 1 94.7 FM
+https://antenaone.crossradio.com.br/stream/1;?type=.flv
+
+#EXTINF:-1 tvg-logo="https://img.radios.com.br/radio/lg/radio8803_1578516680.png"group-title="SP",Rádio Transamérica 100.1 FM
+https://25003.live.streamtheworld.com:443/RT_SPAAC.aac?type=.flv
+
+#EXTINF:-1 tvg-logo="https://img.radios.com.br/radio/lg/radio31289_1439401796.jpg" group-title="SP",89 FM Rádio Rock
+https://14013.live.streamtheworld.com:443/RADIO_89FM_ADP.aac
+
+#EXTINF:-1 tvg-logo="https://img.radios.com.br/radio/lg/radio24477_1579261235.jpg" group-title="SP",Rádio BandNews SP 96.9 FM
+https://26503.live.streamtheworld.com/BANDNEWSFM_SPAAC/HLS/05b3785a-6e2f-4919-a995-7e582a1a0bdc/0/playlist.m3u8
+
+#EXTINF:-1 tvg-logo="" group-title="RADIOS",
+
+#EXTINF:-1 tvg-logo="" group-title="RADIOS",
+
+#EXTINF:-1 tvg-logo="" group-title="RADIOS",
+
+#EXTINF:-1 tvg-logo="" group-title="RADIOS",
+
+
+
+### RIO DE JANEIRO #####
+######## RADIOS ######
+
+
+#EXTINF:-1 tvg-logo="https://img.radios.com.br/radio/lg/radio13213_1559217338.png" group-title="RJ",Rádio FM O Dia 100.5
+http://streaming.livespanel.com:20000/live?type=.flv
+
+
+#EXTINF:-1 tvg-logo="" group-title="SP",
+
+#EXTINF:-1 tvg-logo="" group-title="RADIOS",
+
+#EXTINF:-1 tvg-logo="" group-title="RADIOS",
+
+#EXTINF:-1 tvg-logo="" group-title="RADIOS",
+
+#EXTINF:-1 tvg-logo="" group-title="RADIOS",
